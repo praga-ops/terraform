d1b8246b30085423264c190be8d8071e908f4e63
praga-ops <praga.network@gmail.com>
2025-11-20 11:52:14 +0530

    added more outputs

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -8,6 +8,12 @@ module "iam" {
     project_tag = var.project_tag
 }
 
+output "List-of-changes" {
+    value = <<EOT
+    1. This will create a IAM user "${var.admin_user}"
+    EOT
+}
+
 output "admin_user" {
     value = module.iam.admin_user
 }
