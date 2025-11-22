02dfa849dd5826919c6eab38e942a470d01db85d
praga-ops <praga.network@gmail.com>
2025-11-22 17:21:57 +0530

    added new modules

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -5,3 +5,9 @@ main_cidr = "10.0.0.0/16"
 #Kubernetes
 
 kube_subnet = "10.0.1.0/24"
+
+key_pair_name = "ec2_key"
+pub_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCvPvyyu0Di9eKUJxGrlnVc7nr7a7Yf9h/xZmJHTXhSWAce+rrgtSh+smbnml2JSuOUKXLlmI/jhMhAS/xE9RC7NOhryrWj5rcfNYUhBaTFV+D6jkoscdPdVppnmy7+YZOhCXAc8m+kp3Y3HC67nCW2Rkh6sERACzhY1BLliEcpLuPZRCSVdwO+yJ5IAwD+YSqzOEwPkUMuPO1iL5xql3YnWMkt28CYX+5DcOItprX1m85L1S3TcL9rE2u+gYWAAj/x0f3vjJ8W1Uv11o9DaNPEw3g+A1mlBcegSb5UngEl3bSFvVmhgJ14ueUjiLPpdMATbzP/mmc7pAG88msJi1G84eG386rGBQvmNeRsyCg7yJfGBml5HNinUJmmXLsUQ+qLHAC7sRmlH+cIeQKsiuJeI/2YgXV+WyAlpIRdepfUu3S8uLJj08OWgNGUk7TMOixd9LPnc1oj+iA9UGBuOLCtyxUhi9pjCrK2r260SOcdaGQ//whc67MiSjIHIgTPhME= pragadeeshp@pragadeeshs-MacBook-Pro.local"
+
+kube_ami = "ami-0d176f79571d18a8f"
+kube_ec2_type = "t3.small"
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -22,3 +22,34 @@ variable "kube_vpc" {
     description = "This is the VPC for Kubernetes"
     type = string
 }
+
+variable "key_pair_name" {
+    type = string
+    description = "This is to create ssh keys assign to EC2 machines for remote access"
+}
+
+variable "pub_key" {
+    type = string
+    sensitive = true
+    description = "This is to assign the public_key"
+}
+
+variable "kube_ami" {
+    type = string
+    description = "This is the kubernetes base ami"
+}
+
+variable "kube_ec2_type" {
+    type = string
+    description = "This is the Kubernetes ec2 instance type"
+}
+
+variable "kube_ec2_subnet" {
+    type = string
+    description = "This is the Kubernetes cluster subnet"
+}
+
+variable "kube_key_name" {
+    type = string
+    description = "This is the base kubernetes key to access EC2"
+}
